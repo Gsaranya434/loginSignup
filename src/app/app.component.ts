9df4d6049148b0ac8ce9c034bafc56e70fc595a8
Gsaranya434 <saranya16434@gmail.com>
2023-03-06 13:42:02 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  textList = [];
   title = 'test-project';
+  public error: any;
+  currentText = "";
+
+
+  loadText(){  
+    var data = (<HTMLInputElement>document.getElementById("intext")).value;
+    // this.textList.push(data);
+    console.log(data);
+    debugger
+  }
+
+  textDrag(){
+
+  }
+  textDelete(){
+
+  }
+  textAdd(){
+
+  }
 }
+
+
